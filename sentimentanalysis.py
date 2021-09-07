6784e65c905ad3d8a0a7ae9ee6d684b68378009b
sagron16 <89860995+sagron16@users.noreply.github.com>
2021-09-06 22:51:34 -0500

    Added code

---
+++ sentimentanalysis.py
@@ -0,0 +1,27 @@
+import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from nltk import tokenize
+
+# Read input.txt
+readfile = open("input.txt","r")
+text = readfile.read()
+
+# Tokenize text
+sentences = tokenize.sent_tokenize(text)
+
+# Instantiate SIA and Scores
+SIA = SentimentIntensityAnalyzer()
+overallScore = 0
+positiveScore = 0
+negativeScore = 0
+
+# Sum scores
+for sentence in text:
+    overallScore += SIA.polarity_scores(sentence)['compound']
+    positiveScore += SIA.polarity_scores(sentence)['positive']
+    negativeScore += SIA.polarity_scores(sentence)['negative']
+    
+# Print scores
+print("Overall Score", overallScore/len(text))
+print("Positive Score", positiveScore/len(text))
+print("Negative Score", negativeScore/len(text))
